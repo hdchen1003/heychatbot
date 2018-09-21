db49c247a316c560d7b2bb11dd44681e0f1ebf0b
Hao-Dun Chen <hdchen1003@gmail.com>
2018-09-21 18:32:05 +0800

    first

---
+++ test.js
@@ -0,0 +1,24 @@
+var bot =[]
+    
+
+
+
+bot[0] = {
+ name: "白癡",
+ num: 3
+}
+
+bot[1] = {
+    name: "笨蛋"
+    
+   }
+   bot[3] = {
+    name: "智障",
+    power: "非常智障"
+   }
+
+
+
+for(var i =0 ;i<bot.length;i++){
+    console.log(bot[i]);
+}
---
+++ views/pages/js/socket.js
@@ -0,0 +1,81 @@
+'use strict'
+
+var util = require('util')
+var net = require('net')
+
+function Socket (parent, options) {
+  net.Socket.call(this, options)
+
+  var state = {}
+
+  this._spdyState = state
+
+  state.parent = parent
+
+  this.servername = parent.servername
+  this.npnProtocol = parent.npnProtocol
+  this.alpnProtocol = parent.alpnProtocol
+  this.authorized = parent.authorized
+  this.authorizationError = parent.authorizationError
+  this.encrypted = true
+  this.allowHalfOpen = true
+}
+
+util.inherits(Socket, net.Socket)
+
+module.exports = Socket
+
+var methods = [
+  'renegotiate', 'setMaxSendFragment', 'getTLSTicket', 'setServername',
+  'setSession', 'getPeerCertificate', 'getSession', 'isSessionReused',
+  'getCipher', 'getEphemeralKeyInfo'
+]
+
+methods.forEach(function (method) {
+  Socket.prototype[method] = function methodWrap () {
+    var parent = this._spdyState.parent
+    return parent[method].apply(parent, arguments)
+  }
+})
+
+const express = require('express');
+const app = express();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+
+// 加入線上人數計數
+let onlineCount = 0;
+
+app.get('/', (req, res) => {
+    res.sendFile( __dirname + '/views/index.html');
+});
+
+io.on('connection', (socket) => {
+    // 有連線發生時增加人數
+    onlineCount++;
+    // 發送人數給網頁
+    io.emit("online", onlineCount);
+
+    socket.on("greet", () => {
+        socket.emit("greet", onlineCount);
+    });
+
+    socket.on("send", (msg) => {
+        // 如果 msg 內容鍵值小於 2 等於是訊息傳送不完全
+        // 因此我們直接 return ，終止函式執行。
+        if (Object.keys(msg).length < 2) return;
+
+        // 廣播訊息到聊天室
+        io.emit("msg", msg);
+    });
+
+    socket.on('disconnect', () => {
+        // 有人離線了，扣人
+        onlineCount = (onlineCount < 0) ? 0 : onlineCount-=1;
+        io.emit("online", onlineCount);
+    });
+});
+
+server.listen(3000, () => {
+    console.log("Server Started. http://localhost:3000");
+});
