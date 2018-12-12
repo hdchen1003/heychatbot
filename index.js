ca5f6e19953e76d208047858e4bc3445f60e1e18
hdchen <hdchen1003@gmail.com>
2018-12-12 23:03:46 +0800

    sss

--- index.js
+++ index.js
@@ -691,10 +691,11 @@ app.get('/nearby', function (req, res) {
 
 
             if (rating != undefined) {
-              Gplace_str += '<tr><td>' + data2.result.name + '<br>'
-              Gplace_str += '評分：' + rating + '<br/>電話：' + phone + '<br/>類型：' + type + '<br/>地址：' + address + '<br/>評論：' + review + '<br/>'
+              Gplace_str += '<div class="near_title"><h3>' + data2.result.name + '</h3>'
+              Gplace_str += '<div style=text-align:left><h6>評分：' + rating + '</h6><h6>電話：' + phone + '</h6><h6>類型：' + type + '</h6><h6>地址：' + address + '</h6><h6>評論：' + review + '</h6>'
               Gplace_str += '<form action="http://' + ip + '/addTOfavorite_nearby" method="POST"><input type="hidden" name="other" value="<a href=https://www.google.com/maps/place/'+encodeURI(data2.result.name)+'/@'+data2.result.geometry.location.lat + ',' + data2.result.geometry.location.lng+',17z>查看地圖</a>"><input type="hidden" name="value" value="' + review + '"><input type="hidden" name="address" value="' + address + '"><input type="hidden" name="type" value="' + type + '"><input type="hidden" name="phone" value="' + phone + '"><input type="hidden" name="arrnum" value="' + data2.result.name + '"><button type="submit"> <img src="img/7pF0p0K.jpg" height="10" width="10" alt=""> </button></form> '
-              Gplace_str += '</tr></td>'
+              // Gplace_str += '</tr></td>'
+              Gplace_str +=  '</div></div>'
             }
           })
         }
